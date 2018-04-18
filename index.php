7c4462538dae94a7cc0088ae2fbb4530ee7b4311
Ezerioha Somto <motivatedsomto@gmail.com>
2018-04-18 10:23:21 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,249 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Material Design Bootstrap</title>
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- Material Design Bootstrap -->
+    <link href="css/mdb.min.css" rel="stylesheet">
+    <!-- Your custom styles (optional) -->
+    <!--link href="css/style.css" rel="stylesheet"-->
+</head>
+
+<body>
+    <!--main navigation-->
+    <header>
+
+        <!--Navbar-->
+        <nav class="navbar navbar-expand-lg navbar-dark indigo fixed-top scrolling-navbar">
+            <div class="container">
+                     <!-- Navbar brand -->
+                <a class="navbar-brand" href="#">Template I</a>
+
+                <!-- Collapse button -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="       navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <!-- Collapsible content -->
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+                    <!-- Links -->
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Pricing</a>
+                        </li>
+
+                        <!-- Dropdown -->
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Members</a>
+                            <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                                <a class="dropdown-item" href="#">CEO</a>
+                                <a class="dropdown-item" href="#">Managing Directors</a>
+                                <a class="dropdown-item" href="#">Departments</a>
+                            </div>
+                        </li>
+
+                    </ul>
+                    <!-- Links -->
+
+                    <!-- Search form -->
+                    <form class="form-inline">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Search" aria-label="Search">
+                    </form>
+                </div>
+                <!-- Collapsible content -->   
+            </div>
+
+           
+        </nav>
+        <!--/.Navbar-->
+        
+    </header>
+    <!--main navigation-->
+    <br>
+    <!--main layout-->
+    <main class="mt-5">
+        <!--main container-->
+        <div class="container">
+            <!--grid row-->
+            <div class="row">
+               <!--grid column-->
+               <div class="col-md-7 mb-4">
+                    <div class="view overlay hm-white-light z-depth-1-half">
+                        <img src="img/c.jpg" class="img-fluid" alt="nature">
+                        <div class="mask"></div>
+                    </div>
+                    
+                   
+               </div>
+               <!--grid column-->
+
+               <!--grid column-->
+               <div class="col-md-5 mb-4">
+                    <h2>Some awesome heading</h2>
+                    <hr>
+                    <p>
+                        Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
+                    </p>
+                    <a href="https://google.com" class="btn btn-indigo">Get it now!</a>
+
+                   
+               </div>
+               <!--grid column-->
+            </div>
+            <!--grid row-->
+            
+            <!--grid row-->
+            <div class="row">
+                <!--grid column-->
+               <div class="col-lg-4 col-md-12 mb-4">
+                                <!--Card-->
+                    <div class="card">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/b.jpg" class="img-fluid" alt="">
+                            <a href="#">
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+
+                        <!--Card content-->
+                        <div class="card-body">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-indigo">Button</a>
+                        </div>
+
+                    </div>
+                    <!--/.Card-->    
+               </div>
+               <!--grid column-->
+               <!--grid column-->
+               <div class="col-lg-4 col-md-6 mb-4">
+                   <!--Card-->
+                    <div class="card">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/d.jpg" class="img-fluid" alt="">
+                            <a href="#">
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+
+                        <!--Card content-->
+                        <div class="card-body">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-indigo">Button</a>
+                        </div>
+
+                    </div>
+                    <!--/.Card-->
+
+               </div>
+               <!--grid column-->
+                 <!--grid column-->
+               <div class="col-lg-4 col-md-6 mb-4">
+                    <!--Card-->
+                    <div class="card">
+
+                        <!--Card image-->
+                        <div class="view overlay hm-white-slight">
+                            <img src="img/header.jpg" class="img-fluid" alt="">
+                            <a href="#">
+                                <div class="mask"></div>
+                            </a>
+                        </div>
+
+                        <!--Card content-->
+                        <div class="card-body">
+                            <!--Title-->
+                            <h4 class="card-title">Card title</h4>
+                            <!--Text-->
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-indigo">Button</a>
+                        </div>
+
+                    </div>
+                    <!--/.Card-->
+
+               </div>
+               <!--grid column-->
+            </div>
+            <!--grid row-->
+        </div>
+        <!--main container-->
+    </main>
+    <!--main layout-->
+
+    <!--footer-->
+    <footer class="page-footer indigo  center-on-small-only">
+
+        <!--Footer Links-->
+        <div class="container">
+            <div class="row">
+
+                <!--First column-->
+                <div class="col-md-6">
+                    <h5 class="title">Location</h5>
+                    <p>Here you can use rows and columns here to organize your footer content.</p>
+                </div>
+                <!--/.First column-->
+
+                <!--Second column-->
+                <div class="col-md-6">
+                    <h5 class="title">Connect with us</h5>
+                    <ul>
+                        <li><a href="https://www.facebook.com"><i class="fa fa-facebook fa-2x mb-3 sr-contact"></i></a></li>
+                        <li><a href="https://www.twitter.com"><i class="fa fa-twitter fa-2x mb-3 sr-contact"></i></a></li>
+                        <li><a href="https://www.linkedin.com"><i class="fa fa-linkedin fa-2x mb-3 sr-contact"></i></a></li>
+                       <li><a href="https://www.instagram.com"><i class="fa fa-instagram fa-2x mb-3 sr-contact"></i></a></li>
+                        
+                    </ul>
+                </div>
+                <!--/.Second column-->
+            </div>
+        </div>
+        <!--/.Footer Links-->
+
+        <!--Copyright-->
+        <div class="footer-copyright">
+            <div class="container-fluid">
+                © 2018 Copyright: <a href="motivatedsomto.blogspot.com"> Ezerioha Somto </a>
+
+            </div>
+        </div>
+        <!--/.Copyright-->
+    </footer>
+    <!--footer-->
+    <!-- SCRIPTS -->
+    <!-- JQuery -->
+    <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+    <!-- Bootstrap tooltips -->
+    <script type="text/javascript" src="js/popper.min.js"></script>
+    <!-- Bootstrap core JavaScript -->
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <!-- MDB core JavaScript -->
+    <script type="text/javascript" src="js/mdb.min.js"></script>
+</body>
+
+</html>
